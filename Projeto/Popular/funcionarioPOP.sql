4fc6cb0fc7ffcea11a8b42235c7e0a35fa2aa048
Unknown <edvaldoer4@gmail.com>
2017-08-16 20:44:02 -0300

    atts

--- Projeto/Popular/funcionarioPOP.sql
+++ Projeto/Popular/funcionarioPOP.sql
@@ -3,12 +3,12 @@ use assistech;
 -- desabilita a checagem de FOREIGN KEY
 SET FOREIGN_KEY_CHECKS = 0;
 
-LOAD DATA LOCAL INFILE "Bibliotecas\\Documentos\\Excel_BD.ods - CONTRACHEQUE.csv"
+LOAD DATA LOCAL INFILE "C:\\Users\\EraNetbook\\Desktop\\BASE_PROBLEMAS_KB.csv"
 INTO TABLE CONTRACHEQUE
-FIELDS ENCLOSED BY '\'' TERMINATED BY ','
+FIELDS ENCLOSED BY '\'' TERMINATED BY ';'
 ;
 
-select * from SERVICO;
+select * from BASEOU_EM;
 
 -- habilita de novo a checagem de restrições de chaves estrangeiras
 SET FOREIGN_KEY_CHECKS = 1;
