aa4a50c1eea9d9929096679b6595420174a1d5f7
Unknown <edvaldoer4@gmail.com>
2017-08-18 21:35:38 -0300

    Help_Desk_1_9

--- Projeto/Popular/funcionarioPOP.sql
+++ Projeto/Popular/funcionarioPOP.sql
@@ -8,7 +8,7 @@ INTO TABLE CONTRACHEQUE
 FIELDS ENCLOSED BY '\'' TERMINATED BY ';'
 ;
 
-select * from INSUMO;
+select * from TV;
 
 -- habilita de novo a checagem de restrições de chaves estrangeiras
 SET FOREIGN_KEY_CHECKS = 1;
